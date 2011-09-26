dae752db4555095128c1b2cd2e2bd851308db6b0
App <app@BE-MAC-C3.local>
2011-09-26 09:51:50 +0800

    Initial Commit

---
+++ HellowWorldTests/HellowWorldTests.h
@@ -0,0 +1,17 @@
+//
+//  HellowWorldTests.h
+//  HellowWorldTests
+//
+//  Created by App on 2011/9/26.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface HellowWorldTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ HellowWorldTests/HellowWorldTests.m
@@ -0,0 +1,33 @@
+//
+//  HellowWorldTests.m
+//  HellowWorldTests
+//
+//  Created by App on 2011/9/26.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "HellowWorldTests.h"
+
+
+@implementation HellowWorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HellowWorldTests");
+}
+
+@end
